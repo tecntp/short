14a8ea8da2fb49104234c6a1aa06b2152e3f8480
tecntp <79776371+tecntp@users.noreply.github.com>
2021-05-28 08:51:40 +0200

    Add files via upload

---
+++ short.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  
+var count= 12;
+var counter=setInterval(timer, 1000);
+var timerDiv = document.getElementById('timer');
+var link = document.getElementById('linktimer');
+  
+function timer()
+  
+{
+count=count-1;
+if (count <= 0)
+{
+clearInterval(counter);
+link.style.display = '';
+timerDiv.innerHTML="";
+return;
+}
+timerDiv.innerHTML=count + "";
+}
+}
